2c00fc82d9c09c73c0918cf83d0f3a867ce83843
Raven <1772133019@qq.com>
2022-09-26 14:27:29 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import mysql.connector
+
+connection = mysql.connector.connect(host='localhost',
+                                     port='3306',
+                                     user='root',
+                                     password='TstwTdtct42'
+                                     )
+
+cursor = connection.cursor()
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
